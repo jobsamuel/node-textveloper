3f6311a626671df2ec908f67a71cfa2145044c86
Jobsamuel Núñez <jobsamuel.nunez@outlook.com>
2015-10-28 11:11:16 -0430

    Se reescribió la clase Textveloper a ES6

--- textveloper.js
+++ textveloper.js
@@ -2,133 +2,135 @@
 
 import request from 'request';
 
-function Textveloper({ cuentaToken, subcuentaToken }) {
-	if (!cuentaToken || !subcuentaToken) {
-		throw new Error('Debe colocar los parámetros cuentaToken y subcuentaToken.');
-	} else if (typeof cuentaToken !== 'string' || typeof subcuentaToken !== 'string') {
-		throw new Error('Los parámetros cuentaToken y subcuentaToken deben ser string.');
+class Textveloper {
+	constructor({ cuentaToken, subcuentaToken }) {
+		if (!cuentaToken || !subcuentaToken) {
+			throw new Error('Debe colocar los parámetros cuentaToken y subcuentaToken.');
+		} else if (typeof cuentaToken !== 'string' || typeof subcuentaToken !== 'string') {
+			throw new Error('Los parámetros cuentaToken y subcuentaToken deben ser string.');
+		}
+
+		this._cuentaToken =  cuentaToken;
+		this._subcuentaToken = subcuentaToken;
 	}
 
-	this._cuentaToken =  cuentaToken;
-	this._subcuentaToken = subcuentaToken;
-}
+	enviar({ telefono, mensaje }, callback) {
+		if (!telefono || !mensaje) {
+			throw new Error('Debe colocar los parámetros telefono y mensaje para enviar un SMS.');
+		} else if (typeof telefono !== 'string' || typeof mensaje !== 'string') {
+			const error = new Error('Los parámetros telefono y mensaje deben ser string.');
+			callback(error);
+		} else if (!telefono.startsWith('0')) {
+			const mensaje = 'El parámetro telefono debe poseer obligatoriamente ' +
+				'uno de los siguientes prefijos: 0414, 0424, 0426, 0416.';
+			const error = new Error(mensaje);
+			callback(error);
+		}
 
-Textveloper.prototype.enviar = function({ telefono, mensaje }, callback) {
-	if (!telefono || !mensaje) {
-		throw new Error('Debe colocar los parámetros telefono y mensaje para enviar un SMS.');
-	} else if (typeof telefono !== 'string' || typeof mensaje !== 'string') {
-		const error = new Error('Los parámetros telefono y mensaje deben ser string.');
-		callback(error);
-	} else if (!telefono.startsWith('0')) {
-		const mensaje = 'El parámetro telefono debe poseer obligatoriamente ' +
-			'uno de los siguientes prefijos: 0414, 0424, 0426, 0416.';
-		const error = new Error(mensaje);
-		callback(error);
+		const form = {
+			cuenta_token: this._cuentaToken,
+			subcuenta_token: this._subcuentaToken,
+			telefono, 
+			mensaje 
+		};
+		const config = {
+			url: 'http://api.textveloper.com/enviar/',
+			method: 'POST',
+			form
+		};
+
+		request(config, function(err, response, body) {
+			if (err) {
+				const error = new Error(err);
+				return callback(error); 
+			} else if (response.statusCode !== 200) {
+				const error = new Error(body);
+				return callback(error);
+			} else {
+				return callback(null, body);
+			}
+		});
 	}
 
-	const form = {
-		cuenta_token: this._cuentaToken,
-		subcuenta_token: this._subcuentaToken,
-		telefono, 
-		mensaje 
-	};
-	const config = {
-		url: 'http://api.textveloper.com/enviar/',
-		method: 'POST',
-		form
-	};
+	puntos(cuenta, callback) {
+		const form = {
+			cuenta_token: this._cuentaToken
+		};
+		let url = 'http://api.textveloper.com/saldo-cuenta/';
+		let cb = callback;
 
-	request(config, function(err, response, body) {
-		if (err) {
-			const error = new Error(err);
-			return callback(error); 
-		} else if (response.statusCode !== 200) {
-			const error = new Error(body);
-			return callback(error);
+		if (!cuenta) {
+			throw new Error('El primer argumento debe se un string o un callback function.');
+		} else if (typeof cuenta === 'string') {
+			if (cuenta === 'subcuenta') {
+				url = 'http://api.textveloper.com/saldo-subcuenta/';
+				form.subcuenta_token = this._subcuentaToken;
+			}
+		} else if (typeof cuenta === 'function') {
+			cb = cuenta;
 		} else {
-			return callback(null, body);
+			throw new Error(`Se esperaba un string o un callback function, pero se obtuvo un ${typeof cuenta}`);
 		}
-	});
-}
 
-Textveloper.prototype.puntos = function(cuenta, callback) {
-	const form = {
-		cuenta_token: this._cuentaToken
-	};
-	let url = 'http://api.textveloper.com/saldo-cuenta/';
-	let cb = callback;
+		const config = {
+			method: 'POST',
+			url,
+			form
+		};
 
-	if (!cuenta) {
-		throw new Error('El primer argumento debe se un string o un callback function.');
-	} else if (typeof cuenta === 'string') {
-		if (cuenta === 'subcuenta') {
-			url = 'http://api.textveloper.com/saldo-subcuenta/';
-			form.subcuenta_token = this._subcuentaToken;
-		}
-	} else if (typeof cuenta === 'function') {
-		cb = cuenta;
-	} else {
-		throw new Error(`Se esperaba un string o un callback function, pero se obtuvo un ${typeof cuenta}`);
+		request(config, function(err, response, body) {
+			if (err) {
+				const error = new Error(err);
+				return cb(error); 
+			} else if (response.statusCode !== 200) {
+				const error = new Error(body);
+				return cb(error);
+			} else {
+				return cb(null, body);
+			}
+		});
 	}
 
-	const config = {
-		method: 'POST',
-		url,
-		form
-	};
+	historial(tipo, callback) {
+		const form = {
+			cuenta_token: this._cuentaToken,
+			subcuenta_token: this._subcuentaToken
+		};
+		let url = 'http://api.textveloper.com/historial-envios/';
+		let cb = callback;
 
-	request(config, function(err, response, body) {
-		if (err) {
-			const error = new Error(err);
-			return cb(error); 
-		} else if (response.statusCode !== 200) {
-			const error = new Error(body);
-			return cb(error);
+		if (!tipo) {
+			throw new Error('El primer argumento debe se un string o un callback function.');
+		} else if (typeof tipo === 'string') {
+			if (tipo === 'transferencias') {
+				url = 'http://api.textveloper.com/historial-transferencias/';
+			} else if (tipo === 'compras') {
+				url = 'http://api.textveloper.com/historial-compras/';
+			}
+		} else if (typeof tipo === 'function') {
+			cb = tipo;
 		} else {
-			return cb(null, body);
+			throw new Error(`Se esperaba un string o un callback function, pero se obtuvo un ${typeof tipo}`);
 		}
-	});
-}
 
-Textveloper.prototype.historial = function(tipo, callback) {
-	const form = {
-		cuenta_token: this._cuentaToken,
-		subcuenta_token: this._subcuentaToken
-	};
-	let url = 'http://api.textveloper.com/historial-envios/';
-	let cb = callback;
+		const config = {
+			method: 'POST',
+			url,
+			form
+		};
 
-	if (!tipo) {
-		throw new Error('El primer argumento debe se un string o un callback function.');
-	} else if (typeof tipo === 'string') {
-		if (tipo === 'transferencias') {
-			url = 'http://api.textveloper.com/historial-transferencias/';
-		} else if (tipo === 'compras') {
-			url = 'http://api.textveloper.com/historial-compras/';
-		}
-	} else if (typeof tipo === 'function') {
-		cb = tipo;
-	} else {
-		throw new Error(`Se esperaba un string o un callback function, pero se obtuvo un ${typeof tipo}`);
+		request(config, function(err, response, body) {
+			if (err) {
+				const error = new Error(err);
+				return cb(error); 
+			} else if (response.statusCode !== 200) {
+				const error = new Error(body);
+				return cb(error);
+			} else {
+				return cb(null, body);
+			}
+		});
 	}
-
-	const config = {
-		method: 'POST',
-		url,
-		form
-	};
-
-	request(config, function(err, response, body) {
-		if (err) {
-			const error = new Error(err);
-			return cb(error); 
-		} else if (response.statusCode !== 200) {
-			const error = new Error(body);
-			return cb(error);
-		} else {
-			return cb(null, body);
-		}
-	});
 }
 
 export default Textveloper;
