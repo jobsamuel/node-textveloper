5f71eca841bc7f7df4f2e17eef74d111b68e0d9c
Jobsamuel Núñez <jobsamuel.nunez@outlook.com>
2016-08-14 13:25:59 -0400

    Mejora el manejo de errores
    En lugar de crear un nuevo 'Error', explicar los detalles (en el parámetro 'message' de 'Error') y retornarlo, sólo se retornan los detalles. De ese modo, es más simple entender lo que sucedió y efectuar acciones en base a dicha respuesta.

--- textveloper.js
+++ textveloper.js
@@ -5,15 +5,15 @@ import request from 'request';
 class Textveloper {
 	constructor({ cuenta_token, aplicacion_token }) {
 		if (!cuenta_token || !aplicacion_token) {
-			const err = 'Se requieren los parámetros \'cuenta_token\' y \'aplicacion_token\'.';
+			const msj = 'Se requieren los parámetros \'cuenta_token\' y \'aplicacion_token\'.';
 
-			throw new Error(err);
+			throw new Error(msj);
 		} 
 
 		if (typeof cuenta_token !== 'string' || typeof aplicacion_token !== 'string') {
-			const err = 'Los parámetros \'cuenta_token\' y \'aplicacion_token\' deben ser string.';
+			const msj = 'Los parámetros \'cuenta_token\' y \'aplicacion_token\' deben ser string.';
 
-			throw new Error(err);
+			throw new Error(msj);
 		}
 
 		this._cuenta_token = cuenta_token;
@@ -22,14 +22,13 @@ class Textveloper {
 
 	enviar({ telefono, mensaje }, callback) {
 		if (!telefono || !mensaje) {
-			const err = 'Se requieren los parámetros \'telefono\' y \'mensaje\' para enviar un SMS.';
+			const msj = 'Se requieren los parámetros \'telefono\' y \'mensaje\' para enviar un SMS.';
 
-			throw new Error(err);
+			throw new Error(msj);
 		} 
 
 		if (typeof telefono !== 'string' || typeof mensaje !== 'string') {
-			const _error = new Error(_mensaje);
-			_error.message = JSON.stringify({
+			const _error = JSON.stringify({
 				mensaje: 'El \'telefono\' y/o \'mensaje\' son inválidos.',
 				codigo: 'PARAMETROS_INVALIDOS',
 				detalle: `Se esperaba que 'telefono' y 'mensaje' fuesen string, pero ` +
@@ -40,8 +39,7 @@ class Textveloper {
 		} 
 
 		if (!telefono.startsWith('0')) {
-			const _error = new Error();
-			_error.message = JSON.stringify({
+			const _error = JSON.stringify({
 				mensaje: 'Número telefónico inválido.',
 				codigo: 'TELEFONO_INVALIDO',
 				detalle: 'El parámetro \'telefono\' debe poseer obligatoriamente ' +
@@ -66,8 +64,7 @@ class Textveloper {
 
 		request(config, function(error, response, body) {
 			if (error) {
-				const _error = new Error();
-				_error.message = JSON.stringify({
+				const _error = JSON.stringify({
 					mensaje: 'Falló intento de envío de SMS.',
 					codigo: 'SMS_FALLIDO',
 					detalle: error.message
@@ -77,11 +74,10 @@ class Textveloper {
 			} 
 
 			if (response.statusCode !== 200) {
-				const _error = new Error();
-				_error.message = JSON.stringify({
+				const _error = JSON.stringify({
 					mensaje: 'El SMS no pudo ser procesado.',
 					codigo: 'SMS_NO_PROCESADO',
-					detalle: JSON.parse(body).detalle
+					detalle: JSON.parse(body).detalle || 'n/a'
 				});
 
 				return callback(_error);
@@ -111,8 +107,7 @@ class Textveloper {
 
 		request(config, function(error, response, body) {
 			if (error) {
-				const _error = new Error();
-				_error.message = JSON.stringify({
+				const _error = JSON.stringify({
 					mensaje: 'Falló intento de consulta de información.',
 					codigo: 'CONSULTA_FALLIDA',
 					detalle: error.message
@@ -122,11 +117,10 @@ class Textveloper {
 			}
 
 			if (response.statusCode !== 200) {
-				const _error = new Error();
-				_error.message = JSON.stringify({
+				const _error = JSON.stringify({
 					mensaje: 'La consulta de información no pudo ser procesada.',
 					codigo: 'CONSULTA_NO_PROCESADA',
-					detalle: JSON.parse(body).detalle
+					detalle: JSON.parse(body).detalle || 'n/a'
 				});
 
 				return callback(_error);
@@ -143,34 +137,33 @@ class Textveloper {
 
 	historial(opciones, callback) {
 		if (!opciones || !callback) {
-			const err = 'Se requieren los argumentos \'opciones\' y \'callback\' para consultar el historial.';
+			const msj = 'Se requieren los argumentos \'opciones\' y \'callback\' para consultar el historial.';
 
-			throw new Error(err);
+			throw new Error(msj);
 		}
 
 		if (typeof opciones === 'object' && !opciones.telefono) {
-			const err = 'Se requiere el parámetro \'teléfono\' para consultar el historial.';
+			const msj = 'Se requiere el parámetro \'teléfono\' para consultar el historial.';
 
-			throw new Error(err);
+			throw new Error(msj);
 		}
 
 		if (typeof opciones !== 'object' && typeof opciones !== 'string') {
-			const err = `Se esperaba que el argumento 'opciones' fuese object o string, ` + 
+			const msj = `Se esperaba que el argumento 'opciones' fuese object o string, ` +
 				`pero se recibió ${typeof opciones}.`;
 
-			throw new Error(err);
+			throw new Error(msj);
 		}
 
 		if (typeof callback !== 'function') {
-			const err = `Se esperaba que el argumento 'callback' fuese function, ` +
+			const msj = `Se esperaba que el argumento 'callback' fuese function, ` +
 				`pero se recibió ${typeof callback}.`;
 
-			throw new Error(err);
+			throw new Error(msj);
 		}
 
 		if ((opciones.telefono && !opciones.telefono.startsWith('0')) || (typeof opciones === 'string' && !opciones.startsWith('0'))) {
-			const _error = new Error();
-			_error.message = JSON.stringify({
+			const _error = JSON.stringify({
 				mensaje: 'Número telefónico inválido.',
 				codigo: 'TELEFONO_INVALIDO',
 				detalle: 'El teléfono debe poseer obligatoriamente uno de los siguientes ' +
@@ -202,8 +195,7 @@ class Textveloper {
 
 		request(config, function(error, response, body) {
 			if (error) {
-				const _error = new Error();
-				_error.message = JSON.stringify({
+				const _error = JSON.stringify({
 					mensaje: 'Falló intento de consulta del historial.',
 					codigo: 'CONSULTA_FALLIDA',
 					detalle: error.message
@@ -213,11 +205,10 @@ class Textveloper {
 			}
 
 			if (response.statusCode !== 200) {
-				const _error = new Error();
-				_error.message = JSON.stringify({
+				const _error = JSON.stringify({
 					mensaje: 'La consulta del historial no pudo ser procesada.',
 					codigo: 'CONSULTA_NO_PROCESADA',
-					detalle: JSON.parse(body).detalle
+					detalle: JSON.parse(body).detalle || 'n/a'
 				});
 
 				return callback(_error);
